6c16be11bba6a563fd8b3f46f38cc006627e7f7a
DV-dongseop <ehdtjq1278@naver.com>
2023-11-18 22:54:22 +0900

    초기세팅

---
+++ StudyMatching_smashing/App.js
@@ -0,0 +1,20 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>리액트 네이티브 최초 테스트</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
