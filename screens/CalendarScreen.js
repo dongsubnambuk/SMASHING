ec9abf07cd6320fb6bf513a0e3fdb8547fa1ea3a
rudfkr <rudfkr248@gmail.com>
2024-01-21 00:31:54 +0900

    스케줄 디자인 완성

--- screens/CalendarScreen.js
+++ screens/CalendarScreen.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import { StyleSheet, View, Text } from 'react-native';
 import { Calendar } from 'react-native-calendars';
 import { format } from 'date-fns';
+import { Entypo } from '@expo/vector-icons';
 
 export default function CalendarScreen({ navigation }) {
   const posts = [
@@ -9,19 +10,19 @@ export default function CalendarScreen({ navigation }) {
       id: 1,
       title: '제목입니다.',
       contents: '내용입니다.',
-      date: '2024-01-26',
+      date: '2024-01-02',
     },
     {
       id: 2,
       title: '제목입니다.',
       contents: '내용입니다.',
-      date: '2024-01-27',
+      date: '2024-01-04',
     },
   ];
 
   const markedDates = posts.reduce((acc, current) => {
     const formattedDate = format(new Date(current.date), 'yyyy-MM-dd');
-    acc[formattedDate] = { marked: true };
+    acc[formattedDate] = { marked: true, dotColor: 'red' };
     return acc;
   }, {});
 
@@ -36,14 +37,6 @@ export default function CalendarScreen({ navigation }) {
 
   return (
     <View style={styles.container}>
-      <View style={styles.subHeader}>
-        <Text style={{fontSize: 20, fontWeight: '900'}}>
-          나의 일정
-        </Text>
-        <Text style={{fontSize: 10, fontWeight: "500"}}>
-          소중한 기록들...
-        </Text>
-      </View>
       <Calendar
         style={styles.calendar}
         markedDates={markedSelectedDates}
@@ -57,26 +50,101 @@ export default function CalendarScreen({ navigation }) {
           setSelectedDate(new Date(day.dateString));
         }}
       />
+      <View style={styles.scheduleContainer}>
+        <Text style={styles.scheduleContainerText}>
+          This Month's Study Schedule
+        </Text>
+      </View>
+      <View style={styles.schedule}>
+        <View style={styles.scheduleDate}>
+          <Text style={{fontSize: 18, fontWeight: '600', color: '#FFB800'}}>
+            2
+          </Text>
+        </View>
+        <View style={styles.scheduleName}>
+          <Text style={{fontSize: 18, fontWeight: '600'}}>
+            리액트 스터디
+          </Text>
+        </View>
+        <View style={styles.scheduleInfo}>
+          <Entypo name="triangle-right" size={60} color="#FFFAEE" />
+        </View>
+      </View>
+      <View style={{...styles.schedule, backgroundColor: '#FFD9D9'}}>
+        <View style={styles.scheduleDate}>
+          <Text style={{fontSize: 18, fontWeight: '600', color: '#FF0000'}}>
+            4
+          </Text>
+        </View>
+        <View style={styles.scheduleName}>
+          <Text style={{fontSize: 18, fontWeight: '600'}}>
+            엑스포 스터디
+          </Text>
+        </View>
+        <View style={styles.scheduleInfo}>
+          <Entypo name="triangle-right" size={60} color="#FFFAEE" />
+        </View>
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    backgroundColor: "grey"
+  },
+  scheduleContainer: {
+    alignItems: 'center',
+    paddingVertical: 8,
+    marginHorizontal: 20,
+    backgroundColor: '#3D4AE7',
+    borderRadius: 10,
+  },
+  scheduleContainerText: {
+    color: 'white',
+    fontSize: 15,
+    fontWeight: '600',
   },
   calendar: {
     marginHorizontal: 20,
-    marginTop: 10,
-    marginBottom: 20,
+    marginVertical: 10,
     width: '90%',
     paddingVertical: 10,
-    borderRadius: 10,
+    borderRadius: 15,
+    elevation: 10, // 그림자
   },
-  subHeader: {
+  schedule: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    backgroundColor: '#FFE6A8',
+    marginTop: 10,
+    marginHorizontal: 20,
+    paddingHorizontal: 10,
+    borderRadius: 12,
+    alignItems: 'center',
+  },
+  scheduleDate: {
+    paddingVertical: 10,
+    paddingHorizontal: 12,
+    backgroundColor: '#FFFAEE',
+    borderRadius: 13,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  scheduleName: {
+    paddingVertical: 10,
+    paddingHorizontal: 80,
+    backgroundColor: '#FFFAEE',
+    borderRadius: 13,
+    justifyContent: 'center',
+  },
+  scheduleInfo: {
+    marginTop: -2,
+    marginHorizontal: -13,
+  },
+  /*  subHeader: { 
     backgroundColor: 'white',
     paddingVertical: 5,
     paddingHorizontal: 20,
     fontSize: 80,
-  }
+  } */
 });
